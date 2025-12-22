8992c8345011f22b5eb390463b497635d1a1ec8e
zsdtlucky <14130477+zsdtlucky@user.noreply.gitee.com>
2025-12-22 20:55:21 +0800

    feat:增加楼栋管理模块

---
+++ base-web/src/main/java/com/zjgsu/cyd/baseweb/web/house_building/mapper/HouseBuildingMapper.java
@@ -0,0 +1,7 @@
+package com.zjgsu.cyd.baseweb.web.house_building.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zjgsu.cyd.baseweb.web.house_building.entity.HouseBuilding;
+
+public interface HouseBuildingMapper extends BaseMapper<HouseBuilding> {
+}
